a8681c5386929408587a698546c2a4c2c06389fe
Cleiton <cleiton.rcruz@gmail.com>
2025-07-01 14:13:15 -0300

    Segundo

--- primeiro_programa_java/src/cursojava/executavel/PrimeiraClasseJava.java
+++ primeiro_programa_java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -5,7 +5,8 @@ public class PrimeiraClasseJava {
 	public static void main(String[] args) {
 		
 		System.out.println("Meu primeiro programa em Java.");
-
+		// hehe
+		System.out.println("Programa em Java.");
 	}
 
 }
