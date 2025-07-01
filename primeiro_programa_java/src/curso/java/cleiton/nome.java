46a8cd5f2e386b293c7f8cedfb49cc066be10400
Cleiton <cleiton.rcruz@gmail.com>
2025-07-01 14:37:44 -0300

    Nova classe nome

---
+++ primeiro_programa_java/src/curso/java/cleiton/nome.java
@@ -0,0 +1,30 @@
+package curso.java.cleiton;
+
+public class nome {
+
+	private String nome;
+	private String sobrenome;
+
+	public nome(String nome, String sobrenome) {
+		super();
+		this.nome = nome;
+		this.sobrenome = sobrenome;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSobrenome() {
+		return sobrenome;
+	}
+
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+
+}
